6710794f9be075b500ba9c43f57dd223544464a7
vitormsantanaa <vitormanoelssz1704@gmail.com>
2026-03-17 09:30:11 -0300

    FEAT: Criando chave estrangeira para Categoria

--- core/models/livro.py
+++ core/models/livro.py
@@ -1,4 +1,6 @@
 from django.db import models
+from .categoria import Categoria
+
 
 
 class Livro(models.Model):
@@ -6,6 +8,7 @@ class Livro(models.Model):
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantidade = models.CharField(default=0, null=True, blank=True)
     preco = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name='livros', null=True, blank=True)
 
     def __str__(self):
         return f'({self.id}) ({self.titulo})'
